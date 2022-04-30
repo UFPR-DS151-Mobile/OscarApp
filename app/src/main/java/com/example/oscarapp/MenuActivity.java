0a313420fbe45ac6cebddfb9d76b6563870d6430
gustavobkrm <gustavobkrm@gmail.com>
2022-04-30 13:01:16 -0300

    menu comunication, oscar logo and activity organization

--- app/src/main/java/com/example/oscarapp/MenuActivity.java
+++ app/src/main/java/com/example/oscarapp/MenuActivity.java
@@ -3,9 +3,14 @@ package com.example.oscarapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.oscarapp.activity.ConfirmarVoto;
+import com.example.oscarapp.activity.Diretor;
+import com.example.oscarapp.activity.Filmes;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -28,14 +33,18 @@ public class MenuActivity extends AppCompatActivity {
 
         switch (item.getItemId()) {
             case R.id.votarFilme:
+                Intent intentFilmes = new Intent(getApplicationContext(), Filmes.class);
+                startActivity(intentFilmes);
                 break;
 
             case R.id.votarDiretor:
-
+                Intent intentDiretor = new Intent(getApplicationContext(), Diretor.class);
+                startActivity(intentDiretor);
                 break;
 
             case R.id.confirmarVoto:
-
+                Intent intentConfirmarVoto = new Intent(getApplicationContext(), ConfirmarVoto.class);
+                startActivity(intentConfirmarVoto);
                 break;
 
             case R.id.logout:
---
+++ app/src/main/java/com/example/oscarapp/activity/Diretor.java
@@ -0,0 +1,16 @@
+package com.example.oscarapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.oscarapp.R;
+
+public class Diretor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diretor);
+    }
+}
